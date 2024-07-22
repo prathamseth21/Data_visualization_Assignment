7afd8119b10b6d442079344bc264f8151673435c
PRATHAM SETH <147477634+prathamseth21@users.noreply.github.com>
2024-07-22 16:57:06 +0530

    Update lab Assignment3.py

--- lab Assignment3.py
+++ lab Assignment3.py
@@ -1,1 +1,45 @@
+# 1 ZeroDivisionError
+print('line 1')
+print('line 2')
+print('line 3')
+print('line 4')
+try:
+    print(10/0)
+except ZeroDivisionError:
+    print("You cannot divide by zero")
+print('line 5')
+print('line 6')
+print('line 7')
 
+
+# 2 ValueError
+try:    
+    print(int(input("Enter a integer value ")))
+except ValueError:
+    print("Please enter a valid integer value")
+
+
+# 3 FileNotFoundError
+print('line 1')
+print('line 2')
+print('line 3')
+print('line 4')
+try:
+    print(10/0)
+
+except ZeroDivisionError:
+    print("You cannot divide by zero")
+try:
+  open("abc.txt")
+except FileNotFoundError:
+    print("File not found")
+print('line 5')
+print('line 6')
+print('line 7')
+
+# 4 Type Error
+try:    
+    print(int(input("Enter first integer value ")))
+    print(int(input("Enter second integer value ")))
+except ValueError:
+    print("Please enter a valid integer value")
